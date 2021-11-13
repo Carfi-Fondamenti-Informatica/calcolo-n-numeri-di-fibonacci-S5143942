6f5e831103e0b20b4e826ab003b34ace2d699aef
S5143942 <92802547+S5143942@users.noreply.github.com>
2021-11-13 19:19:03 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,21 +2,18 @@
 using namespace std;
 
 int main() {
-   int n=0;
+   int n=0,i=0,j=1,k=1,l=1;
    cin>>n;
-   
-   if (n<2){
-      cout<<"errore"<<endl;
-      
+   if (n>1){
+   cout<<1<<endl;
+      for (l=0;l<n-1;l++){
+      k=i+j;
+  cout<<k<<endl;
+         i=j;
+         j=k;
+         }
   }else{
-      cout<<"1"<<endl;
-      int a=0, b=1, c=0;
-      for (int i=0; i<=(n-2); i++);
-      cout<<c;
-     { c=a+b;
-         a=b;
-         b=c;
-     }
+      cout<<"errore"<<endl;
    }
    return 0;
 }
